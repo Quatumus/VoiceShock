fd4fd6b9136a39e59ff5f73f2144752841f8f31f
Quatumus <joona.puhakainen@gmail.com>
2025-04-30 20:35:31 +0300

    Work on database

---
+++ VoiceShock/Config/WordListConfig.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections.Generic;
+using VoiceShock.Config;
+using System.ComponentModel;
+
+public enum ControlType
+{
+    Stop = 0,
+    Shock = 1,
+    Vibrate = 2,
+    Sound = 3
+}
+
+namespace VoiceShock.Config
+{
+    public class Word
+    {
+        public int Id { get; set; }
+        public string Text { get; set; }
+        public List<WordShocker> WordShockers { get; set; }
+    }
+
+    public class Shocker
+    {
+        public int Id { get; set; }
+        public string ShockerID { get; set; }
+        public bool Enabled { get; set; }
+        public List<WordShocker> WordShockers { get; set; }
+    }
+
+    public class WordShocker
+    {
+        public int WordId { get; set; }
+        public Word Word { get; set; }
+
+        public int ShockerId { get; set; }
+        public Shocker Shocker { get; set; }
+
+        public int Duration { get; set; }
+        public int Intensity { get; set; }
+        public ControlType ControlType { get; set; }
+        public ControlType Warning { get; set; }
+    }
+
+}
---
+++ VoiceShock/Helpers/AccountHelper.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using Microsoft.Data.Sqlite;
+using VoiceShock.Config;
+using System.Diagnostics;
+using System.Threading.Tasks;
+
+namespace VoiceShock.Data
+{
+    static class AccountHelper
+    {
+        private static bool _hasBeenInitialized = false;
+
+        private static async Task Init()
+        {
+            if (_hasBeenInitialized)
+                return;
+
+            await using var connection = new SqliteConnection(Constants.DatabasePath);
+            await connection.OpenAsync();
+
+            try
+            {
+                var createTableCmd = connection.CreateCommand();
+                createTableCmd.CommandText = @" 
+                CREATE TABLE IF NOT EXISTS Account (
+                Backend TEXT PRIMARY KEY,
+                Password TEXT,
+                Email TEXT,
+                Token TEXT
+                );";
+                await createTableCmd.ExecuteNonQueryAsync();
+            }
+            catch (Exception e)
+            {
+                Debug.Print(e.Message);
+                throw;
+            }
+
+            _hasBeenInitialized = true;
+        }
+
+        public static async Task<AccountConfig?> LoadAsync()
+        {
+            await Init();
+            await using var connection = new SqliteConnection(Constants.DatabasePath);
+            await connection.OpenAsync();
+
+            var selectCmd = connection.CreateCommand();
+            selectCmd.CommandText = "SELECT * FROM Account";
+
+            await using var reader = await selectCmd.ExecuteReaderAsync();
+            if (await reader.ReadAsync())
+            {
+                return new AccountConfig
+                {
+                    Token = reader.GetString(0),
+                    Password = reader.GetString(1),
+                    Email = reader.GetString(2),
+                    Backend = new Uri(reader.GetString(3))
+                };
+            }
+            else
+            {
+                return null;
+            }
+
+        }
+
+        public static async Task<int> SaveItemAsync(AccountConfig item)
+        {
+            await Init();
+            await using var connection = new SqliteConnection(Constants.DatabasePath);
+            await connection.OpenAsync();
+
+            // delete old data
+            var deleteCmd = connection.CreateCommand();
+            deleteCmd.CommandText = "DELETE FROM Account";
+            await deleteCmd.ExecuteNonQueryAsync();
+
+            // save new data
+            var saveCmd = connection.CreateCommand();
+            saveCmd.CommandText = @"
+            INSERT OR REPLACE INTO Account (Backend, Password, Email, Token)
+            VALUES (@Backend, @Password, @Email, @Token);";
+
+            saveCmd.Parameters.AddWithValue("@Token", item.Token);
+            saveCmd.Parameters.AddWithValue("@Password", item.Password);
+            saveCmd.Parameters.AddWithValue("@Email", item.Email);
+            saveCmd.Parameters.AddWithValue("@Backend", item.Backend?.ToString());
+
+            return await saveCmd.ExecuteNonQueryAsync();
+        }
+
+        public static async Task<int> DeleteItemAsync(string item)
+        {
+            await Init();
+            await using var connection = new SqliteConnection(Constants.DatabasePath);
+            await connection.OpenAsync();
+
+            var deleteCmd = connection.CreateCommand();
+            deleteCmd.CommandText = "DELETE FROM Account WHERE Backend = @item";
+            deleteCmd.Parameters.AddWithValue("@item", item);
+
+            return await deleteCmd.ExecuteNonQueryAsync();
+        }
+
+        public static async Task DropTableAsync()
+        {
+            await Init();
+            await using var connection = new SqliteConnection(Constants.DatabasePath);
+            await connection.OpenAsync();
+
+            var dropTableCmd = connection.CreateCommand();
+            dropTableCmd.CommandText = "DROP TABLE IF EXISTS Account";
+
+            await dropTableCmd.ExecuteNonQueryAsync();
+            _hasBeenInitialized = false;
+        }
+    }
+}
